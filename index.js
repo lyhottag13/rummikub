8f112a4b9052eba3b1aa4bb1cee36207769c7a32
lyhottag13 <leonardo@promoevt.com>
2025-12-24 22:56:19 -0700

    Updated Logic

---
+++ index.js
@@ -0,0 +1,28 @@
+let interval;
+const input = document.getElementById('input');
+
+function main() {
+    document.addEventListener('mousedown', e => {
+        if (e.target === document.getElementById('app')) {
+            resetTimer();
+        }
+    });
+    input.addEventListener('input', () => {
+        console.log(input.value);
+        input.value = input.value.replaceAll(/[^0-9]/g, '');
+    });
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    let count = input.value;
+    document.getElementById('timer').textContent = count--;
+    interval = setInterval(() => {
+        if (count <= 0) {
+            clearInterval(interval);
+        }
+        document.getElementById('timer').textContent = count--;
+    }, 1000);
+}
+
+main();
